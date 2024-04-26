cfd00c2dcc61a7600ac07c763b9036dabae38921
josterga <josterga@uci.edu>
2024-04-25 22:23:24 -0700

    Fixing Session Storage

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,9 +26,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "7cee833ec037438f16758961f4cfe6d9",
-"/": "7cee833ec037438f16758961f4cfe6d9",
-"main.dart.js": "8743e2a26972bef269394e372fc83092",
+"index.html": "c9b1505f8a606bbe2ad7af3a711df624",
+"/": "c9b1505f8a606bbe2ad7af3a711df624",
+"main.dart.js": "f0429fc47cd31615bb1288c379ef9184",
 "manifest.json": "e701041868d5f9db715679c7c552277d",
 "version.json": "4b77ff9f9a6a1cdda664b4962177657a"};
 // The application shell files that are downloaded before a service worker can
